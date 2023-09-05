a4bd017504970f27b6c1a1a52585707cffe63e85
Isoken Ibizugbe <isokenjune@gmail.com>
2023-09-05 21:42:11 +0000

    corrections to functions in accordance to pycodestyle

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 #define function first
-def power(a, b)
-result = a ** b
-return (result)
+def power(a, b):
+    result = a ** b
+    return (result)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last_digit = abs(num) % 10
+    return (last_digit)
