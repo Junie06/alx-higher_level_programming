828152be20d564b39b8d12ec6355be2369351952
Isoken Ibizugbe <isokenjune@gmail.com>
2023-09-07 23:37:09 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     list_names = dir(hidden_4)
     for i in list_names:
         if list_names[:2] != "_":
-        print(list_names)
+        print("{}".format(list_names))
