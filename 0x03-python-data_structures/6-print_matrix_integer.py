f2b0e2c738b278bc27973bcf33aab101fbbed39e
Isoken Ibizugbe <isokenjune@gmail.com>
2023-09-12 16:10:45 +0000

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in row:
-            print("{:d}".format(i), end='')
-            if row(i) != row[-1]:
-                print(" ", end='')
-            else:
-                print()
+        if len(row) != 0:
+            for i in row:
+                print("{:d}".format(i), end='')
+                if row.index(i) != len(row) - 1:
+                    print(" ", end='')
+                else:
+                    print()
