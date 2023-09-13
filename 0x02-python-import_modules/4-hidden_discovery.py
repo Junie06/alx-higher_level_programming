35a94ad2d5fedde00d63fe8c958f7e1fc72c339b
Isoken Ibizugbe <isokenjune@gmail.com>
2023-09-13 11:19:51 +0000

    corrections for task 4 and magic calculator

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     list_names = dir(hidden_4)
     for name in list_names:
-        if list_names[:2] != "__":
+        if name[:2] != "__":
             print("{}".format(name))
