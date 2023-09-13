c67bfa7141c9a6d99c691f255e851f9654fa2977
Isoken Ibizugbe <isokenjune@gmail.com>
2023-09-13 22:35:49 +0000

    update to tasks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square_matrix = [[number ** 2 for num in row] for row in matrix]
-    return square_matrix
+    return [[num ** 2 for num in row] for row in matrix]
