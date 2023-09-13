67a75a2c681d45bf91f0354fe8f9a78b0de89ef7
Isoken Ibizugbe <isokenjune@gmail.com>
2023-09-13 22:43:52 +0000

    fixed pycodestyle for tasks 0-7

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = set(my_list)
+    my_list = set(my_list)
     total = 0
-    for number in my list:
+    for number in my_list:
         total += number
-    return (num)
+    return (total)
