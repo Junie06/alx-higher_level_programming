3bb04790dd93edac2cd21536b44183583d657014
Isoken Ibizugbe <isokenjune@gmail.com>
2023-09-27 23:04:48 +0000

    solved pycodestyle issues

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,8 +8,8 @@ class Square:
     '''
 
     def __init__(self, size=0, position=(0, 0)):
-            self.size = size
-            self.position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
