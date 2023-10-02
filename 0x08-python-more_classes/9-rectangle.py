5640c17948430eaff7463301f0679499d5706d57
Isoken June Ibizugbe <isokenjune@gmail.com>
2023-10-02 23:51:17 +0000

    correction and task 9 included

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -5,9 +5,13 @@
 class Rectangle:
     '''Defines properties of a rectangle'''
 
+    number_of_instances = 0
+    print_symbol = '#'
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -47,10 +51,28 @@ class Rectangle:
         for i in range(self.area()):
             if i and not (i % self.__width):
                 print()
-            print('#', end='')
+            print(self.print_symbol, end='')
         return ''
 
     def __repr__(self):
         a = str(self.__width)
         b = str(self.__height)
         return "Rectangle (" + a + ", " + b + ")"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        return (cls(size, size))
