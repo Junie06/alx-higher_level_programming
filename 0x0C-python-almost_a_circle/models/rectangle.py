f7acc8e2616890134bd6b526adcb1a2c26f780e4
Isoken June Ibizugbe <isokenjune@gmail.com>
2023-10-17 12:51:10 +0100

    corrections to syntax

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -43,7 +43,7 @@ class Rectangle(Base):
         def width(self, val):
             if not type(val, int):
                 raise TypeError("width must be an integer")
-            if width <= 0:
+            if val <= 0:
                 raise ValueError("width must be > 0")
             self.__width = val
 
@@ -51,7 +51,7 @@ class Rectangle(Base):
         def height(self, val):
             if not type(val, int):
                 raise TypeError("height must be an integer")
-            if height <= 0:
+            if val <= 0:
                 raise ValueError("height must be > 0")
             self.__height = val
 
@@ -59,7 +59,7 @@ class Rectangle(Base):
         def x(self, val):
             if not type(val, int):
                 raise TypeError("x must be an integer")
-            if value < 0:
+            if val < 0:
                 raise ValueError("x must be >= 0")
             self.__x = val
 
@@ -67,7 +67,7 @@ class Rectangle(Base):
         def y(self, val):
             if not type(val, int):
                 raise TypeError("y must be an integer")
-            if value < 0:
+            if val  < 0:
                 raise ValueError("y must be >= 0")
             self.__y = val
 
