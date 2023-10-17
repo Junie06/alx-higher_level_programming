305c3514fd279c5d5fecc3b05fea53cf7ec1dee8
Isoken June Ibizugbe <isokenjune@gmail.com>
2023-10-17 11:36:02 +0100

    instantiate base id

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,7 +15,7 @@ class Base:
 
         """
         if id is not None:
-            self.id
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
